71ed30411d053635e1fcac5b321368c67b7b763a
hhaa1382 <mehranfarhamid82@gmail.com>
2022-10-21 00:47:47 +0330

    scroll pane part add

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,15 +1,17 @@
 import Data.Read;
 import Elements.*;
+import Pages.MusicListPart;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        MyLinkList<Music> musics= Read.readMusics();
-        for(int i=0;i<musics.size();i++){
-            Music m=musics.get(i);
-            System.out.println("Artist name : "+m.getArtistName()+"   track name : "+m.getTrackName()+"   date : "+m.getReleaseDate());
-        }
+//        MyLinkList<Music> musics= Read.readMusics();
+//        for(int i=0;i<musics.size();i++){
+//            Music m=musics.get(i);
+//            System.out.println("Artist name : "+m.getArtistName()+"   track name : "+m.getTrackName()+"   date : "+m.getReleaseDate());
+//        }
+        new MusicListPart();
     }
 }
--- project/src/Pages/MusicListPart.java
+++ project/src/Pages/MusicListPart.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MusicListPart extends JFrame {
     public MusicListPart(){
@@ -9,7 +10,17 @@ public class MusicListPart extends JFrame {
         this.setSize(600,600);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
+        this.setLayout(null);
 
+        DefaultListModel<String> values=new DefaultListModel<>();
+        JList<String> list=new JList<>(values);
+
+        JScrollPane panel=new JScrollPane(list);
+        panel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        panel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        panel.setBounds(10,10,500,400);
+
+        this.getContentPane().add(panel);
         this.setVisible(true);
     }
 }
