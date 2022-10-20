49ce75f164bdcfa8f3bf241ae9f098303606eeb0
hhaa1382 <mehranfarhamid82@gmail.com>
2022-10-20 16:31:38 +0330

    node class add

--- project/src/MyLinkList.java
+++ project/src/MyLinkList.java
@@ -1,2 +1,27 @@
 public class MyLinkList {
+    private class Node{
+        private Node next;
+        private Music music;
+
+        Node(Node next,Music music){
+            this.next=next;
+            this.music=music;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Music getMusic() {
+            return music;
+        }
+
+        public void setMusic(Music music) {
+            this.music = music;
+        }
+    }
 }
