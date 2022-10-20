121ad69d6a217fc6a3287a68324f2701f49ce81c
hhaa1382 <mehranfarhamid82@gmail.com>
2022-10-20 16:56:05 +0330

    link list functions add

--- project/src/MyLinkList.java
+++ project/src/MyLinkList.java
@@ -1,27 +1,81 @@
-public class MyLinkList {
-    private class Node{
-        private Node next;
-        private Music music;
+public class MyLinkList<E>{
+    private static class Node<E>{
+        Node<E> next;
+        E element;
 
-        Node(Node next,Music music){
+        Node(E element,Node<E> next){
             this.next=next;
-            this.music=music;
+            this.element=element;
         }
 
-        public Node getNext() {
+        public Node<E> getNext() {
             return next;
         }
 
-        public void setNext(Node next) {
+        public void setNext(Node<E> next) {
             this.next = next;
         }
 
-        public Music getMusic() {
-            return music;
+        public E getElement() {
+            return element;
         }
 
-        public void setMusic(Music music) {
-            this.music = music;
+        public void setElement(E element) {
+            this.element = element;
         }
+    }
+
+    private int size=0;
+    private Node<E> head;
+    private Node<E> tail;
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public E first(){
+        if(isEmpty())   return null;
+        return head.getElement();
+    }
+
+    public E last(){
+        if(isEmpty())   return null;
+        return head.getElement();
+    }
+
+    public void addFirst(E element){
+        head= new Node<E>(element,head);
+        if(isEmpty()){
+            tail=head;
+        }
+
+        size++;
+    }
+
+    public void addLast(E element){
+        Node<E> hold=new Node<>(element,null);
+        if(isEmpty()){
+            head=hold;
+        }
+        else{
+            tail.setNext(hold);
+        }
+        tail=hold;
+        size++;
+    }
+
+    public E removeFirst(E element){
+        if(isEmpty())   return null;
+        E hold=head.getElement();
+        head=head.getNext();
+        size--;
+        if(size==0){
+            tail=null;
+        }
+        return hold;
     }
 }
