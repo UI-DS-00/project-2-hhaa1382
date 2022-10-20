12e38ebbbf7ecaec1734f498daac5e57a478d4c7
hhaa1382 <mehranfarhamid82@gmail.com>
2022-10-21 00:33:00 +0330

    music list page add

--- project/src/Main.java
+++ project/src/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         MyLinkList<Music> musics= Read.readMusics();
         for(int i=0;i<musics.size();i++){
             Music m=musics.get(i);
---
+++ project/src/Pages/MusicListPart.java
@@ -0,0 +1,15 @@
+package Pages;
+
+import javax.swing.*;
+
+public class MusicListPart extends JFrame {
+    public MusicListPart(){
+        this.setTitle("Music Player");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600,600);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
+        this.setVisible(true);
+    }
+}
