ab876aeba8c1000a882767c48c64104b4324567c
hhaa1382 <mehranfarhamid82@gmail.com>
2022-10-23 23:50:01 +0330

    useless comments delete

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,5 +1,4 @@
 import Data.Read;
-import Data.Write;
 import Elements.*;
 import Pages.MusicListPart;
 
@@ -9,8 +8,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         MyLinkList<Music> musics= Read.readMusics();
-//        MyLinkList<PlayList> playLists=Read.readPlayLists(musics);
-//        System.out.println(playLists.size());
         new MusicListPart(musics);
     }
 }
